57f9da3ff8204460fa74f9ff0cf6039b5c274ae1
Cynthia Nikolai <cynthianikolai@gmail.com>
2020-09-15 14:56:19 -0400

    Initial Commit

---
+++ shamaz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shamaz
+//
+//  Created by Cynthia Nikolai on 9/15/20.
+//  Copyright © 2020 Cynthia Nikolai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
